f5996a2b9fbd8416832162f9960833c3f869fc73
JuanZzz4 <149608503+JuanZzz4@users.noreply.github.com>
2024-01-07 22:49:52 -0500

    Add files via upload

---
+++ Intento dos/main.py
@@ -0,0 +1,56 @@
+import re
+import random
+
+def get_response(user_input):
+    split_message = re.split(r'\s|[,:;.?!-_]\s*', user_input.lower())
+    response = check_all_messages(split_message)
+    return response
+
+def message_probability(user_message, recognized_words, single_response=False, required_word=[]):
+    message_certainty = 0
+    has_required_words = True
+
+    for word in user_message:
+        if word in recognized_words:
+            message_certainty +=1
+
+    percentage = float(message_certainty) / float (len(recognized_words))
+
+    for word in required_word:
+        if word not in user_message:
+            has_required_words = False
+            break
+    if has_required_words or single_response:
+        return int(percentage * 100)
+    else:
+        return 0
+
+def check_all_messages(message):
+        highest_prob = {}
+
+        def response(bot_response, list_of_words, single_response = False, required_words = []):
+            nonlocal highest_prob
+            highest_prob[bot_response] = message_probability(message, list_of_words, single_response, required_words)
+
+        response('Hola', ['hola', 'klk', 'saludos', 'buenas'], single_response = True)
+        response('Estoy bien y tu?', ['como', 'estas', 'va', 'vas', 'sientes'], required_words=['como'])
+        response('Siempre a la orden', ['gracias', 'te lo agradezco', 'thanks'], single_response=True)
+        response('¿Cómo puedes manejar el estrés?', ['estres', 'manejar', 'controlar', 'calmar'], single_response=True)
+        response('Practicar la meditación diaria puede ayudar a reducir el estrés.', ['meditación', 'reducir', 'calmar', 'tranquilizar'])
+        response('Hacer ejercicio regularmente es una excelente manera de liberar el estrés.', ['ejercicio', 'deporte', 'actividad física', 'liberar estrés'])
+        response('Asegúrate de tomarte tiempo para descansar y relajarte.', ['descanso', 'relajación', 'tiempo para uno mismo', 'desconectar'], single_response=True)
+        response('Habla con amigos o seres queridos sobre lo que te preocupa.', ['hablar', 'conversar', 'expresar', 'compartir'], single_response=True)
+
+
+    
+        best_match = max(highest_prob, key=highest_prob.get)
+        #print(highest_prob)
+
+        return unknown() if highest_prob[best_match] < 1 else best_match
+
+def unknown():
+    response = ['puedes decirlo de nuevo?', 'No estoy seguro de lo quieres', 'búscalo en google a ver que tal'][random.randrange(3)]
+    return response
+
+while True:
+    print("Bot: " + get_response(input('You: ')))
